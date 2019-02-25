998b26a4ede2adfe9273725120c04e0c92b5d2ab
Mohamed Emad <m.emad550@gmail.com>
2019-02-25 12:43:52 +0200

    add one life to player after each 100 point

--- js/app.js
+++ js/app.js
@@ -5,24 +5,29 @@ const modal = document.querySelector('.modal'),
     dashScore = document.querySelector('.score'),
     modalScore = document.getElementById('score');
 
-let lives,
-    score;
+let score;
 
-// Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+
+
+// Random function to calculate speed of the enemy
+function getRandomSpeed(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+// Enemies that must our player must avoid
+var Enemy = function (x, y) {
     // Variables applied to each of our instances go here,
     this.x = x;
     this.y = y;
-    this.speed = speed;
+    this.speed = getRandomSpeed(1, 10);
     this.width = 50;
     this.height = 85;
-    // The image/sprite for our enemies, this uses
-    this.sprite = 'images/enemy-bug.png';
+    // The image/soul for our enemies, this uses
+    this.soul = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     if (this.x > 500) {
         this.x = -100;
     } else {
@@ -31,67 +36,80 @@ Enemy.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.soul), this.x, this.y);
 };
+
+
 // Start Player object
-var Player = function(x, y) {
-    // Variables applied to each of our instances go here,
-    this.x = x;
-    this.y = y;
-    this.width = 75;
-    this.height = 85;
-    // The image/sprite for our player, this uses
-    this.sprite = 'images/char-boy.png';
-};
-
-// Parameter: dt, a time delta between ticks
-Player.prototype.update = function(dt) {
-    if (player.y === -35 ) {
-        addScore();   
+class Player {
+    constructor(x, y) {
+        // Variables applied to each of our instances go here
+        // The image/soul for our player, this uses, the default soul is the boy
+        this.soul = 'images/char-boy.png';
+        this.x = x;
+        this.y = y;
+        this.width = 75;
+        this.height = 85;
+        // player lifes
+        this.life = 5;
     }
-    
-    playAgain.addEventListener('click',()=>{
-        init();
-        player.x = 200;
-        player.y = 390;
-        modal.style.display = "none";
-    })
-};
-
-Player.prototype.handleInput = function(keyCode) {
-    switch(keyCode) {
-        case "up":
-            if(this.y > -35) {
-                this.y -= 85;
-            }
-        break;
-        case "down":
-            if(this.y < 390) {
-                this.y += 85;
-            }
-        break;
-        case "left":
-            if(this.x > 0) {
-                this.x -= 100;
-            }
-        break;
-        case "right":
-            if(this.x < 400 ) {
-                this.x += 100;
-            }
-        break;
+    // Draw the Player on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.soul), this.x, this.y);
+    };
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        if (player.y === -35) {
+            addScore();
+        }
+        playAgain.addEventListener('click', () => {
+            init();
+            reset();
+            modal.style.display = "none";
+        });
+    }
+    // handle player movement
+    handleInput(keyCode) {
+        switch (keyCode) {
+            case "up":
+                if (this.y > -35) {
+                    this.y -= 85;
+                }
+            break;
+            case "down":
+                if (this.y < 390) {
+                    this.y += 85;
+                }
+            break;
+            case "left":
+                if (this.x > 0) {
+                    this.x -= 100;
+                }
+            break;
+            case "right":
+                if (this.x < 400) {
+                    this.x += 100;
+                }
+            break;
+        }
     }
 };
 
-// Draw the enemy on the screen, required method for game
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+function select(){
+    const characters = document.querySelectorAll(".character");
+    for (let i = 0; i < characters.length; i++) {
+        // Loop over Character Images and Change the Selected one based on a 'Click' event
+        characters[i].addEventListener("click", () => {
+            // Change the player image
+            player.soul = "'" + characters[i].getAttribute("src") + "'";
+            player.update();
+        });
+    }
+}
 
-// This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -100,21 +118,19 @@ document.addEventListener('keyup', function(e) {
     };
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
 // check Collisions function.
 function checkCollisions() {
-    allEnemies.forEach(function(enemy) {
-        if (player.x < enemy.x + enemy.width && player.x + player.width > enemy.x &&
-            player.y < enemy.y + enemy.height && player.height + player.y > enemy.y) {
-            
-            lives --;
+    allEnemies.forEach(function (enemy) {
+        if (player.x < (enemy.x + enemy.width) && (player.x + player.width) > enemy.x && player.y < (enemy.y + enemy.height) && (player.height + player.y) > enemy.y) {
+            player.life--;
             player.y = 390;
-            if (lives === 0 ){
-                lives = 0;
+            if (player.life === 0) {
                 modal.style.display = "block";
                 dashScore.innerHTML = score;
                 modalScore.innerHTML = score;
             }
-            life.innerHTML = `X${lives}`
+            life.innerHTML = `X ${player.life}`
         }
     });
 }
@@ -124,29 +140,38 @@ function addScore() {
     dashScore.innerHTML = score;
     player.x = 200;
     player.y = 390;
+    if (score % 100 == 0 ){
+        player.life++
+        life.innerHTML = `X ${player.life}`;
+    }
 }
-// Now instantiate your objects.
+
+
 // Place all enemy objects in an array called allEnemies
 let allEnemies = [],
-    yArray = [50,50,50,50,50,50,50,135,135,135,135,135,135,135,135,135,220,220,220,220,220,220,220,220,220,220,220,220];
-function init(){
-    lives = 3;
+    yArray = [50, 50, 50, 50, 50, 50, 50, 135, 135, 135, 135, 135, 135, 135, 135, 135, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220];
+
+// initialize the game
+function init() {
     score = 0;
-    life.innerHTML = `X${lives}`;
+    life.innerHTML = `X ${player.life}`;
     dashScore.innerHTML = score;
     allEnemies = [];
     for (let i = 0; i < 5; i++) {
         let x = -50,
-            y = yArray[Math.floor(Math.random() * yArray.length)],
-            speed = Math.floor(Math.random() * 11);
-            // making sure speed will not equal 0
-            if (speed === 0){
-                speed += 5;
-            }
-        allEnemies.push(new Enemy(x, y, speed));
+            y = yArray[Math.floor(Math.random() * yArray.length)];
+        allEnemies.push(new Enemy(x, y));
     }
-    
 }
+// reset the player data when it dies
+function reset() {
+    player.x = 200;
+    player.y = 390;
+    player.life = 5;
+    life.innerHTML = `X ${player.life}`;
+}
+
 // Place the player object in a variable called player
-init();
 var player = new Player(200, 390);
+init();
+
